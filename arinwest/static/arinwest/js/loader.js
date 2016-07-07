bfbbedd245bfb7ff49f247faa17d4ceb017ac8ff
Isaev Nikita <isv.nikita@gmail.com>
2016-07-07 16:25:00 +0300

    add first edit

--- arinwest/static/arinwest/js/loader.js
+++ arinwest/static/arinwest/js/loader.js
@@ -4,6 +4,15 @@ var main = function() {
     $('.staff-member').hide();
     var code = $('.sorting-menu a').parent().parent().find('.active').attr('data-code');
     $('.staff-member' + code).show();
+    $.ajax({
+            url: '/getinsta',
+            type: 'POST',   
+            data: {"filial" : code},
+            
+            success: function(response) {
+                $('.insta-block').html(response);
+            },
+        });
 
     $('.sorting-menu a').click(function() {
         if($(this).hasClass('active')) return false;
@@ -13,7 +22,7 @@ var main = function() {
         $(this).closest('.sorting-menu').find('.responsive-filtration-title .text').text($(this).text());
         var filterValue = $(this).attr('data-code');
         var width = $('.staff-member' + prev_code).css("width");
-        /*$.ajax({
+        $.ajax({
             url: '/getinsta',
             type: 'POST',   
             data: {"filial" : filterValue},
@@ -21,7 +30,7 @@ var main = function() {
             success: function(response) {
                 $('.insta-block').html(response);
             },
-        });*/
+        });
         $('.staff-member').hide();
         $('.staff-member'+filterValue).css("width", width);
         $('.staff-member'+filterValue).show();
